b3f689742e06839088cc74767f5f53bdfc37de88
songxiangfeng <jakesong1018@aliyun.com>
2018-06-11 18:22:57 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,22 @@
-var gulp = require('gulp'),
-    babel = require('gulp-babel');
+let gulp = require('gulp'),
+    babel = require('gulp-babel'),
+    clean = require('gulp-clean'),
+    uglify = require('gulp-uglify');
 
-gulp.task('default', function() {
-    // es6 -> es5
+let jsDist = './dist/js/';
+
+gulp.task('clean', () => {
+    gulp.src('./dist')
+        .pipe(clean());
+});
+gulp.task('babel', () => {
     gulp.src('./src/js/*.js')
         .pipe(babel())
-        .pipe(gulp.dest('./dist/js/'));
+        .pipe(gulp.dest(jsDist));
 });
+gulp.task('uglify', ['babel'], () => {
+    gulp.src('./dist/js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'));
+});
+gulp.task('default', ['clean', 'babel']);
