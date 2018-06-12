cd55c0d87649b1e49c0527bd070216c16b3d7206
songxiangfeng <jakesong1018@aliyun.com>
2018-06-12 12:00:55 +0800

    压缩

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,31 @@
 let gulp = require('gulp'),
     babel = require('gulp-babel'),
     clean = require('gulp-clean'),
-    uglify = require('gulp-uglify');
-
-let jsDist = './dist/js/';
+    uglify = require('gulp-uglify'),
+    uglifycss = require('gulp-uglifycss'),
+    sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('clean', () => {
     gulp.src('./dist')
         .pipe(clean());
 });
-gulp.task('babel', () => {
+gulp.task('minjs', () => {
     gulp.src('./src/js/*.js')
+        .pipe(sourcemaps.init())
         .pipe(babel())
-        .pipe(gulp.dest(jsDist));
+        .pipe(uglify({
+            compress: {
+                drop_console: true
+            }
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./dist/js/'));
 });
-gulp.task('uglify', ['babel'], () => {
-    gulp.src('./dist/js/*.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('./dist/js'));
+gulp.task('mincss', () => {
+    gulp.src('./src/css/*.css')
+        .pipe(uglifycss())
+        .pipe(gulp.dest('./dist/css/'))
 });
-gulp.task('default', ['clean', 'babel']);
+gulp.task('default', ['clean'], () => {
+    gulp.start('minjs', 'mincss');
+});
