9a78dd1eca43f163cf528e4c695288751b040405
songxiangfeng <jakesong1018@aliyun.com>
2018-11-16 19:46:18 +0800

    update

--- src/js/cf-file.js
+++ src/js/cf-file.js
@@ -14,66 +14,60 @@
         return cf;
     }
 
-    function _createFileItem(i, options) {
+    function _createFileItem(options) {
         let item =
             '<div class="codefalse-file-item" style="height: '+options.height+';width: '+options.width+';">' +
             '   <div class="codefalse-file-operation" style="width: '+options.width+';">'+
-            '      <i key="'+i+'" class="codefalse-file-del codefalse-font icon-delete"></i>' +
-            '      <i data-modaal-content-source="" class="codefalse-file-yulan codefalse-font icon-yulan"></i>' +
+            '      <i class="codefalse-file-del codefalse-font icon-delete"></i>' +
+            '      <i class="codefalse-file-yulan codefalse-font icon-yulan"></i>' +
             '   </div>' +
             '   <img src="" />' +
+            '   <input type="hidden" name="'+options.name+'"/>' +
             '</div>';
         return item;
     }
 
     $.fn.codefalseFile = function (options, callback) {
         let _this = $(this);
-        let codefalseId = 'codefalse-file-' + parseInt(Math.random() * 100000 + '');
+
         let defaults = {
             width: '200px',
             height: '200px',
-            uploadType: 'normal'
+            name: 'codefalseFile'
         };
         let fileOptions = $.extend({}, defaults, options);
+        //生成唯一对应ID
+        let codefalseId = 'codefalse-file-' + parseInt(Math.random() * 100000 + '');
 
-        //监控input选择文件
-        let fileArray = [];
         _this.on('change', () => {
             let files = $(this)[0].files;
             for (let i = 0; i < files.length; i++){
                 let file = files[i];
-                fileArray.push(file);
 
-                let item = _createFileItem(i, fileOptions);
+                let item = _createFileItem(fileOptions);
                 let addDom = $('#'+codefalseId).find('.codefalse-file-add');
                 addDom.before(item);
+
                 //绑定modaal
-                $('.codefalse-file-yulan').modaal({
-                    type: 'image',
-                    before_open: function (e) {
-                        let url = window.URL.createObjectURL(file);
-                        console.log('加载：'+url);
-                        e.target.setAttribute('href', url);
-                    },
-                    before_close: function (e) {
-                        let url = e.find('img').attr('src');
-                        console.log('销毁：'+url);
-                        window.URL.revokeObjectURL(url);
-                    }
-                });
+                $('.codefalse-file-yulan').modaal({type: 'image'});
+
                 //监听文件块事件
                 let fileDom = addDom.prev();
                 //图片加载
                 let img = fileDom.find('img');
-                if (fileOptions.uploadType === 'normal') {
-                    let url = window.URL.createObjectURL(file);
-                    img.attr('src', url);
-                    img.on('load', () => {
-                        window.URL.revokeObjectURL(img.attr('src'));
-                    });
-                } else {
-                    //其他类型
-                }
+                let yulan = fileDom.find('i.codefalse-file-yulan');
+                let fileInput = fileDom.find('input');
+
+                let fileReader = new FileReader();
+                fileReader.readAsDataURL(file);
+                fileReader.onload = (e) => {
+                    img.attr('src', e.target.result);
+                    yulan.attr('href', e.target.result);
+                    fileInput.val(e.target.result);
+                };
+
+
+
                 fileDom.on('mouseenter', () => {
                     let fileOperation = fileDom.find('.codefalse-file-operation');
                     fileOperation.show();
@@ -83,30 +77,34 @@
                     fileOperation.hide();
                 });
                 //图片删除
-                fileDom.find('.codefalse-file-del').on('click', () => {
-                    let key = $(this).attr('key');
-                    fileArray.splice(key, 1);
+                fileDom.find('.codefalse-file-del').on('click', function() {
+                    let img = fileDom.find('img');
+                    let src = img.src;
+                    if (typeof(callback) === 'function') {
+                        callback(src);
+                    }
                     fileDom.remove();
                 });
-            }
-            if (typeof (callback) === 'function'){
-                callback(fileArray);
             }
         });
 
         let methods = {
-            init: function () {
+            _init: function () {
                 console.log('start codefalse file......');
                 //隐藏文件选择
                 _this.attr("style", "display:none");
                 //初始化文件选择组件
-                _this.after(_initComponent(codefalseId, fileOptions))
+                _this.after(_initComponent(codefalseId, fileOptions));
                 //监听添加事件
                 $('.codefalse-file-add>i').on('click', () => {
                     _this.trigger('click');
                 });
+            },
+            adapter: function (images) {
+                console.log(images)
             }
         };
-        methods.init();
+        methods._init();
+        return methods;
     }
 })(jQuery);
